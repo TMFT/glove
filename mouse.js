cf1ad812061a9c4eb3af1c62dba901088ff23fb2
asantos3 <andrewskad@gmail.com>
2016-06-08 17:04:09 +0100

    Improve mouse click

--- mouse.js
+++ mouse.js
@@ -29,53 +29,45 @@ board.on("ready", function() {
   imu.on("change", function() {
 
     var acc = this.accelerometer.acceleration*10;
-    var acc_x = ((this.accelerometer.y + 0.035)*100);
-    var acc_y = (this.accelerometer.x*100);
+    var acc_x = this.accelerometer.y*100;
+    var acc_y = this.accelerometer.x*-100;
 
     date1 = date2;
     date2 = new Date().getSeconds() + new Date().getMilliseconds()*0.001;
 
-    var dx = acc_x * Math.pow(date2 - date1, 2) * 100;
-    var dy = acc_y * Math.pow(date2 - date1, 2) * -100;
+    var dx = acc_x * Math.pow(date2 - date1, 2);
+    var dy = acc_y * Math.pow(date2 - date1, 2);
 
-    dx = (dx*(screenWidth/2))/10;
-    dy = (dy*(screenHeight/2))/10;
+    dx = (dx*(screenWidth/2))/0.5;
+    dy = (dy*(screenHeight/2))/0.5;
 
     if (acc > 10) {
       robot.moveMouse(robot.getMousePos().x+dx, robot.getMousePos().y+dy);
     }
-
+    //console.log(robot.getMousePos().x,robot.getMousePos().y,dx,dy);
   });
 var click = false;
   accelerometerE.on("change", function() {
-    accelPitch1 = this.pitch;
-    if (accelPitch1<10) {
-      robot.mouseClick("left",false);
-    }
-    if (accelPitch1>30 && click == false) {
+    accelPitch1 = this.z;
+    if (accelPitch1<0 && click == false) {
       robot.mouseToggle("down","left");
       click = true;
     }
-    if (accelPitch1<30 && click == true) {
+    if (accelPitch1>0.30 && click == true) {
       robot.mouseToggle("up","left");
       click = false;
     }
-    console.log(accelPitch1);
+    console.log(accelPitch1,accelPitch2)
   });
-
   accelerometerD.on("change", function() {
-    accelPitch2 = this.pitch;
-    if (accelPitch2<10) {
-      robot.mouseClick("right",false);
-    }
-    if (accelPitch2>30 && click == false) {
+    accelPitch2 = this.z;
+    if (accelPitch2<0 && click == false) {
       robot.mouseToggle("down","right");
       click = true;
     }
-    if (accelPitch2<30 && click == true) {
+    if (accelPitch2>0.30 && click == true) {
       robot.mouseToggle("up","right");
       click = false;
     }
-    console.log(accelPitch2);/
   });
 });
